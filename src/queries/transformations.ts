e20fa2de004eeab1416e6dae20a4d97dc2fed7c3
Andy Gimma <andrewgimma@protonmail.com>
2022-09-12 09:11:59 -0500

    prettier

--- src/queries/transformations.ts
+++ src/queries/transformations.ts
@@ -25,9 +25,9 @@ function getDistanceFromLatLonInKm(
   const a =
     Math.sin(dLat / 2) * Math.sin(dLat / 2) +
     Math.cos(deg2rad(lat1)) *
-    Math.cos(deg2rad(lat2)) *
-    Math.sin(dLon / 2) *
-    Math.sin(dLon / 2);
+      Math.cos(deg2rad(lat2)) *
+      Math.sin(dLon / 2) *
+      Math.sin(dLon / 2);
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
   const d = R * c; // Distance in km
   return d;
